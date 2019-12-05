c00bc1eb615563e0e55a3e7db513f3e8bd3aa521
Renan Neri - Notebook <renanneri@poli.ufrj.br>
2019-12-04 22:50:10 -0300

    Change in hessian

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ int main()
     Optimization opt = Optimization();
     // opt.derivativeX1(1,1);
     // opt.derivativeX2(1,1);
-    cout << " Quasi-Newton: " << endl;
+    cout << "Newton: " << endl;
     // opt.quasiNewton(60, 70);
-    opt.newton(1,-1);
+    opt.newton(1,1);
     // opt.quasiNewton(-1, 1);
     // opt.gradient(10,-20);
     return 0;
